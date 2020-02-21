52973d0f1c343564a481fcc62c7611e882762d07
Sergi Mínguez Regidor <sergi.minguez@cocreativa.com>
2020-02-21 17:12:03 +0100

    changes in widgets

--- Widgets/Types/ElementFormV2.php
+++ Widgets/Types/ElementFormV2.php
@@ -9,17 +9,16 @@ use Modules\Architect\Entities\Content;
 use Modules\Architect\Entities\ContentField;
 use Modules\Architect\Entities\Language;
 
-class TotalBox extends Widget implements WidgetInterface
+class ElementFormV2 extends Widget implements WidgetInterface
 {
     public $type = 'widget';
-    public $icon = 'fas fa-chart-bar';
-    public $name = 'TOTAL_BOX';
+    public $icon = 'fa fa fa-list-alt';
+    public $name = 'ELEMENT_FORM_V2';
     public $component = 'CommonWidget';
 
     public $fields = [
-        'name' => 'Modules\Architect\Fields\Types\Text',
-        'icon' => 'Modules\Architect\Fields\Types\Text',
-        'url' => 'Modules\Architect\Fields\Types\Url',
+        'title' => 'Modules\Architect\Fields\Types\Text',
+        'redirect' => 'Modules\Architect\Fields\Types\Url'
     ];
 
     public $rules = [
@@ -29,11 +28,11 @@ class TotalBox extends Widget implements WidgetInterface
     public $hidden = false;
 
     public $settings = [
-      'htmlId',
-      'htmlClass',
-      'tableElements',
-      //'hiddenFilter',
-      'conditionalVisibility'
+        'formElementsV2',
+        'collapsable',
+        'collapsed',
+        'hiddenFilter',
+        'conditionalVisibility'
     ];
 }
 ?>
