5bcfce035a3dc8801d5225f4b7b2227cdba861cb
Sergi Mínguez Regidor <sergi.minguez@cocreativa.com>
2020-06-17 14:36:51 +0200

    update menus

--- Resources/views/partials/topbar-menu.blade.php
+++ Resources/views/partials/topbar-menu.blade.php
@@ -1,26 +1,24 @@
 @foreach(config('architect::menu') as $item)
 
     {{-- If current user can show this menu --}}
-    @if(!empty($item['permissions'])) 
-        @HasNotAbilities($item['permissions'])
-            @continue;
-        @endHasNotAbilities
-    @endif 
+    @if(empty($item['permissions']) || has_permission($item['permissions']))
 
-    {{-- Request pattern for display active of not the menu item --}}
-    @if(!empty($item['patterns']))
-        @php
-            $isActive = collect($item['patterns'])->filter(function($pattern){
-                return Request::is($pattern) ? true : false;
-            })->first();
-        @endphp
+        {{-- Request pattern for display active of not the menu item --}}
+        @if(!empty($item['patterns']))
+            @php
+                $isActive = collect($item['patterns'])->filter(function($pattern){
+                    return Request::is($pattern) ? true : false;
+                })->first();
+            @endphp
+        @endif
+
+        {{-- Render the menu item --}}
+        <li class="{{ $isActive ? 'active' : false }}">
+            <a href="{{ route($item['route']) }}">
+                {{ Lang::get($item['label']) }}
+            </a>
+        </li>
+
     @endif
-
-    {{-- Render the menu item --}}
-    <li class="{{ $isActive ? 'active' : false }}">
-        <a href="{{ route($item['route']) }}">
-            {{ Lang::get($item['label']) }}
-        </a>
-    </li>
 
 @endforeach()
