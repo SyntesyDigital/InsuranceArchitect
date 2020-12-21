e2be3738810b6f8db4e8845221c3a65b60cc5654
Sergi Mínguez Regidor <sergiminguez@MacBook-Pro-de-Sergi.local>
2020-12-21 19:06:27 +0100

    update version

--- Resources/views/layouts/jsconst.blade.php
+++ Resources/views/layouts/jsconst.blade.php
@@ -16,5 +16,5 @@
   const ROLES = {!! json_encode(config('roles'), JSON_PRETTY_PRINT) !!};
   const FONTS = {!! json_encode(config('fonts'), JSON_PRETTY_PRINT) !!};
   const ELEMENT_TEMPLATE_FIELDS = {!! json_encode(Modules\Extranet\Services\ElementTemplate\Fields\FieldConfig::get(), JSON_PRETTY_PRINT) !!};
-  
+  const ENV = "{{env('APP_ENV')}}";
 </script>
